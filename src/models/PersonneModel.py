30280550751862a8c4b3e647713635bddd299c1d
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-08 14:09:20 +0200

    add parameter and attributs id

--- src/models/PersonneModel.py
+++ src/models/PersonneModel.py
@@ -1,7 +1,10 @@
 from .BaseModel import BaseModel
 
 class PersonneModel(BaseModel): 
-    def __init__(self, filename: str) -> None:
+    def __init__(self, filename: str, id: int) -> None:
         super().__init__(filename)
+        self.id = id
+        
+    
         
     
