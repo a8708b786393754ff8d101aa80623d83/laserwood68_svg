436a07c6e7da054989342361b4f8b3b63a950878
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-12 11:08:20 +0200

    add object mainview, button,entry and button impliment

---
+++ src/views/MainView.py
@@ -0,0 +1,23 @@
+import tkinter as ttk
+
+
+class MainView(ttk.Frame):
+    def __init__(self, root: ttk.Tk):
+        super().__init__()
+        self.number_ = ttk.StringVar()
+        self.title_ = ttk.StringVar()
+
+        self.root = root
+
+    def number(self):
+        self.label_number = ttk.Label(
+            self.root, text='Numero du personnage: ').grid(row=0)
+        self.entry_number = ttk.Entry(
+            self.root, textvariable=self.number_).grid(row=0, column=1)
+
+    def title(self):
+        self.label_title = ttk.Label(self.root, text='Titre').grid(row=1)
+        self.entry_title = ttk.Entry(self.root, textvariable=self.title_).grid(row=1, column=1)
+
+    def validate(self):
+        self.button_validate = ttk.Button(self.root, text='Validée').grid()
