729068992d07e1a988ff21ae1ede36e4f17a2386
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-12 13:54:36 +0200

    setting entry and aatributs

--- src/views/MainView.py
+++ src/views/MainView.py
@@ -4,7 +4,7 @@ import tkinter as ttk
 class MainView(ttk.Frame):
     def __init__(self, root: ttk.Tk):
         super().__init__()
-        self.number_ = ttk.StringVar()
+        self.numbers_ = ttk.StringVar()
         self.title_ = ttk.StringVar()
 
         self.root = root
@@ -12,16 +12,17 @@ class MainView(ttk.Frame):
     def number(self):
         self.label_number = ttk.Label(
             self.root, text='Numero du personnage: ').grid(row=0)
-        self.entry_number = ttk.Entry(
-            self.root, textvariable=self.number_).grid(row=0, column=1)
+        self.listbox = ttk.Entry(self.root, textvariable=self.numbers_).grid(row=0, column=1)
 
     def title(self):
-        self.label_title = ttk.Label(self.root, text='Titre', width=20).grid(row=1)
-        self.entry_title = ttk.Entry(self.root, textvariable=self.title_).grid(row=1, column=1)
+        self.label_title = ttk.Label(
+            self.root, text='Titre', width=20).grid(row=1)
+        self.entry_title = ttk.Entry(
+            self.root, textvariable=self.title_).grid(row=1, column=1)
 
     def validate(self):
         self.button_validate = ttk.Button(self.root, text='Validée').grid()
-        
-        
-    def preview(self): 
-        self.button_preview = ttk.Button(self.root, texte="Prévisualisée").grid()
+
+    def preview(self):
+        self.button_preview = ttk.Button(
+            self.root, text="Prévisualisée").grid()
