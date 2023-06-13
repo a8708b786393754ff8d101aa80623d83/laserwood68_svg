8533f1984d0497bbb0cda9c719a10777043ce396
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-14 00:08:02 +0200

    mouve initialisation object

--- run.py
+++ run.py
@@ -10,13 +10,14 @@ from src.models.LayerModel import LayerModel
 
 app = AppView()
 main_frame = MainFrame(app)
+element_model = ElementModel()
+element = ElementController(main_frame, element_model)
 
 
 main_frame.number()
 main_frame.title()
-main_frame.validate()
+main_frame.validate(element.cut)
 main_frame.preview()
 
-element = ElementController(main_frame, ElementModel)
 
 app.mainloop()
