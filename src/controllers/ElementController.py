d917d3ce4b966f8919393f5dbba8a497c44aba9e
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-14 00:42:34 +0200

    add import and messagebox, reformatting and delete condition unitile

--- src/controllers/ElementController.py
+++ src/controllers/ElementController.py
@@ -6,14 +6,15 @@ class ElementController(BaseController):
     def __init__(self, frame, model) -> None:
         super().__init__(frame, model)
 
-    def is_number(self, id: str | int):
+    def is_number(self, id: str | int) -> None:
 
         return re.match('^[0-9]*$', id)
 
-    def cut(self):
+    def cut(self) -> None:
         id = self.frame.numbers_.get()
-        if self.is_number(id):
-            print("C'est un nombre")
+        if not self.is_number(id):
+            self.frame.error('Erruer de définition de l\'id',
+                             'Entrez le numero de l\'element')
 
         title = self.frame.title_.get()
 
