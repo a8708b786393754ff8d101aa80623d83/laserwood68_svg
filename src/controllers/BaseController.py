2f9cab991c01475e0a934ef54c59ed31fa8302c4
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-13 19:24:03 +0200

    add bas method in base controller

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,8 +1,13 @@
+from abc import abstractclassmethod
+
+
 class BaseController:
     def __init__(self, frame, model) -> None:
-        self.frame = frame()
-        self.model = model()
+        self.frame = frame
+        self.model = model
 
+    @abstractclassmethod
     def cut(self): pass
-
+    
+    @abstractclassmethod
     def add(self): pass
