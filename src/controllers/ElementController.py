e3b012b3d1cf231a391689bddd85822c7024eff2
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-14 00:07:41 +0200

    impliment cut in view

--- src/controllers/ElementController.py
+++ src/controllers/ElementController.py
@@ -1,5 +1,12 @@
 from .BaseController import BaseController
 
-class ElementController(BaseController): 
+
+class ElementController(BaseController):
     def __init__(self, frame, model) -> None:
         super().__init__(frame, model)
+
+    def cut(self): 
+        title = self.frame.title_.get()
+        id = self.frame.numbers_.get()
+
+    def add(self): pass
