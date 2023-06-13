af3c1a6e138dea12c7708e00d89d657c3025f8dc
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-13 21:09:25 +0200

    impliment the model and controller

--- run.py
+++ run.py
@@ -2,13 +2,21 @@
 from src.views.AppView import AppView
 from src.views.MainFrame import MainFrame
 
+from src.controllers.ElementController import ElementController
+from src.controllers.LayerController import LayerController
+
+from src.models.ElementModel import ElementModel
+from src.models.LayerModel import LayerModel
+
 app = AppView()
 main_frame = MainFrame(app)
+
 
 main_frame.number()
 main_frame.title()
 main_frame.validate()
 main_frame.preview()
 
+element = ElementController(main_frame, ElementModel)
 
 app.mainloop()
