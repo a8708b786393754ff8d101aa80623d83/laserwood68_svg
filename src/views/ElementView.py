dc29d1e63053e85a301d4b4e91c338f58516379d
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-14 14:08:03 +0200

    add method add, set_root and attribut root

--- src/views/ElementView.py
+++ src/views/ElementView.py
@@ -1,4 +1,13 @@
+import tkinter as ttk
 
-class ElementView: 
+
+class ElementView(ttk.Frame):
     def __init__(self) -> None:
-        pass
+        self.root = None
+
+    def set_root(self, root: ttk.Tk):
+        self.root = root
+
+    def add(self, funct_callback: function):
+        self.button_add = ttk.Button(
+            self.root, text='Ajoutée', command=funct_callback).grid()
