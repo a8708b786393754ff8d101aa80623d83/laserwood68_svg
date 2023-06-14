7a3ad60025ed25b9f92485c24c69d78e1a865020
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-14 14:16:29 +0200

    remove file, add method and remorfatting

--- src/views/MainFrame.py
+++ src/views/MainFrame.py
@@ -11,28 +11,38 @@ class MainFrame(ttk.Frame):
         self.root = root
 
     def number(self):
+        
         self.label_number = ttk.Label(
             self.root, text='Numero du personnage: ').grid(row=0)
         self.listbox = ttk.Entry(
             self.root, textvariable=self.numbers_).grid(row=0, column=1)
 
     def title(self):
+        
         self.label_title = ttk.Label(
             self.root, text='Titre', width=20).grid(row=1)
         self.entry_title = ttk.Entry(
             self.root, textvariable=self.title_).grid(row=1, column=1)
 
     def validate(self, funct_callback):
+        
         self.button_validate = ttk.Button(
             self.root, text='Validée', command=funct_callback).grid()
 
+    def add(self, funct_callback: function):
+        
+        self.button_add = ttk.Button(
+            self.root, text='Ajoutée', command=funct_callback).grid()
+
     def preview(self):
+        
         self.button_preview = ttk.Button(
             self.root, text="Prévisualisée").grid()
 
     def error(self, title: str, message: str):
+        
         messagebox.showerror(title, message)
-        
-    def validate_information(self): 
-        
+
+    def validate_information(self):
+
         messagebox.showinfo('Validée', 'La validation a bien été effectuée')
