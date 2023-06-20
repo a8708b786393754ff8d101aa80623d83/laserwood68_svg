41ffbad632a2e795801353c9b223b1301b139855
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-20 13:00:02 +0200

    setting the widht on lable, row in name label and number, setting placement boutton

--- src/views/MainFrame.py
+++ src/views/MainFrame.py
@@ -21,7 +21,7 @@ class MainFrame(ttk.Frame):
         )
 
         self.personnages = {'num': [], 'name': []}
-        self.names = []
+        self.names = [] 
         self.numbers = []
 
     def title(self):
@@ -31,38 +31,39 @@ class MainFrame(ttk.Frame):
             self.root, textvariable=self.title_).grid(row=1, column=1)
 
     def frame(self):
-        label_num_frame = ttk.Label(self.root, text='Numero frame').grid(row=2)
+        label_num_frame = ttk.Label(self.root, text='Numero frame', width=20).grid(row=2)
         self.entry_num_frame = ttk.Entry(
             self.root, textvariable=self.num_frame_)
 
         self.entry_num_frame.grid(row=2, column=1)
 
     def font(self):
-        label_font = ttk.Label(self.root, text='Font').grid(row=3)
+        label_font = ttk.Label(self.root, text='Font', width=20).grid(row=3)
         self.entry_font = ttk.Entry(self.root, textvariable=self.font_)
 
+        self.entry_font.insert(0, '1')
         self.entry_font.grid(row=3, column=1)
 
     def taille(self):
-        label_taille = ttk.Label(self.root, text='Taille').grid(row=4)
+        label_taille = ttk.Label(self.root, text='Taille', width=20).grid(row=4)
         self.entry_taille = ttk.Entry(
             self.root, textvariable=self.taille_)
-        self.entry_taille.insert(0, 10)
 
+        self.entry_taille.insert(0, 10)
         self.entry_taille.grid(row=4, column=1)
 
     def number_personnage(self):
-        row_label_number = 0
+        row_label_number = 5
         column_label_number = 0
 
-        row_entry_number = 0
+        row_entry_number = 5
         column_entry_number = 1
 
-        row_label_name = 0
-        column_label_name = 3
+        row_label_name = 5
+        column_label_name = 2
  
-        row_entry_name = 0
-        column_entry_name = 5
+        row_entry_name = 5
+        column_entry_name = 3
 
         for _ in range(0, 8):
             name = ttk.StringVar()
@@ -93,26 +94,23 @@ class MainFrame(ttk.Frame):
             
 
     def taille_police(self):
-        label_taille_police = ttk.Label(
-            self.root, text='Taille de la police').grid()
-        self.entry_taille_police = ttk.Entry(
-            self.root, textvariable=self.taille_police_)
+        label_taille_police = ttk.Label(self.root, text='Taille de la police', width=20).grid(row=13)
+        self.entry_taille_police = ttk.Entry(self.root, textvariable=self.taille_police_)
         self.entry_taille_police.insert(0, 10)
-        self.entry_taille_police.grid()
+        self.entry_taille_police.grid(row=13, column=1)
 
     def reset_button(self, funct_callback):
-        self.button_reset = ttk.Button(
-            self.root, text='RESET', command=funct_callback).grid()
+        self.button_reset = ttk.Button(self.root, text='RESET', command=funct_callback, width=20).grid(row=14, column=2)
 
     def validate(self, funct_callback):
 
         self.button_validate = ttk.Button(
-            self.root, text='Validée', command=funct_callback).grid()
+            self.root, text='Validée', command=funct_callback, width=20).grid(row=14)
 
     def add_file(self):
 
         self.button_add = ttk.Button(
-            self.root, text='Ajoutée l\'image svg', command=self.__add_file).grid()
+            self.root, text='Ajoutée l\'image svg', command=self.__add_file, width=20).grid(row=14, column=3)
 
     def __add_file(self):
         file = filedialog.askopenfile(
@@ -126,7 +124,7 @@ class MainFrame(ttk.Frame):
     def preview(self):
 
         self.button_preview = ttk.Button(
-            self.root, text="Prévisualisée").grid()
+            self.root, text="Prévisualisée", width=20).grid(row=14, column=1)
 
     def error(self, title: str, message: str):
 
