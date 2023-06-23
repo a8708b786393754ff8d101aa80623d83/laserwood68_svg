5b9852f77c6acf1f37d8cb4a77649466eebe4f44
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-06-23 14:02:59 +0200

    add method set_tile

--- src/controllers/LayerController.py
+++ src/controllers/LayerController.py
@@ -4,3 +4,11 @@ from .BaseController import BaseController
 class LayerController(BaseController):
     def __init__(self, frame, model) -> None:
         super().__init__(frame, model)
+        
+    def add(self): pass 
+    
+    def set_title(self, title: str): 
+        
+        self.model.title = title
+        
+    
